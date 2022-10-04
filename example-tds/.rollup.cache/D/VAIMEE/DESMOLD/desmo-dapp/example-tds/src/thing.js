068647fbbe9d38f1478f470bd91f2ae884b5e03f
FerrariAndrea <ferrari.andrea.95.b10@gmail.com>
2022-10-04 13:28:55 +0200

    tested

--- example-tds/.rollup.cache/D/VAIMEE/DESMOLD/desmo-dapp/example-tds/src/thing.js
+++
@@ -1,7 +0,0 @@
-import activeTemperatureSensors from "./temperatureSensor/tempSensors";
-import activeStopligths from "./stoplight/stopligth";
-import activeSagecontinuum from "./sagecontinuum/sagecontinuum";
-activeTemperatureSensors();
-activeStopligths();
-activeSagecontinuum();
-//# sourceMappingURL=thing.js.map
---
+++ ipfs/README.md
@@ -0,0 +1,6 @@
+# steps
+
+- donwload and install IPFS on your localhost: https://docs.ipfs.tech/install/ipfs-desktop/
+- npm install
+- npx tsc
+- node build/app.js
